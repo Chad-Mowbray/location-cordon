f2532104d9af3ffb906c636115f29daf81e5a56e
Chad-Mowbray <mowbray.chad@gmail.com>
2020-03-08 09:48:41 -0500

    try catch

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
 
 module.exports.sanitizeLocationHeader = function(rawLocationHeader, whitelist) {
+    
+    try {
+        let whitelistRegex;
+        if (whitelist) {
+            whitelistRegex = new RegExp(whitelist[0], whitelist[1] || undefined)
+        } else {
+            whitelistRegex = new RegExp("[^a-zA-Z0-9/+-_:?=]", "g");
+        }
 
-    let whitelistRegex;
-    if (whitelist) {
-        whitelistRegex = new RegExp(whitelist[0], whitelist[1] || undefined)
-    } else {
-        whitelistRegex = new RegExp("[^a-zA-Z0-9/+-_:?=]", "g");
-    }
+        const uriEncodedHeader = encodeURI(rawLocationHeader)
+        const buf = Buffer.from(uriEncodedHeader, 'ascii');
+        const asciiLocation = buf.toString('ascii')
+        const asciiLocationArr = asciiLocation.split('');
+        const blackListCharCodes = [10, 13, 32, 92];
+        const sanitizedArr = asciiLocationArr.map( char => blackListCharCodes.includes(char.charCodeAt()) ? '' : char );
+        const sanitizedLocationHeader = sanitizedArr.join('');
 
-    const uriEncodedHeader = encodeURI(rawLocationHeader)
-    const buf = Buffer.from(uriEncodedHeader, 'ascii');
-    const asciiLocation = buf.toString('ascii')
-    const asciiLocationArr = asciiLocation.split('');
-    const blackListCharCodes = [10, 13, 32, 92];
-    const sanitizedArr = asciiLocationArr.map( char => blackListCharCodes.includes(char.charCodeAt()) ? '' : char );
-    const sanitizedLocationHeader = sanitizedArr.join('');
-
-    if (!whitelistRegex.test(sanitizedLocationHeader)) { 
-        return sanitizedLocationHeader
-    } else {
-        return new Error('Invalid Input')
+        if (!whitelistRegex.test(sanitizedLocationHeader)) { 
+            return sanitizedLocationHeader
+        } else {
+            return new Error('Invalid Input')
+        }
+    } catch(error) {
+        console.error(error)
     }
 };
 
